9d4ac4424dc9365e6735432d62d5c77526ccb5f4
Pavloffff <pavlov.id.2003@gmail.com>
2024-03-23 22:32:46 +0300

    modified  make.sh

--- make.sh
+++ make.sh
@@ -1,17 +1,17 @@
 read CUR_DIR <<< $(pwd)
-PATH_TO_NGINX_CONF="$CUR_DIR/nginx"
+PATH_TO_NGINX_CONF="$CUR_DIR/build"
 
 BUILD_NAME=with_neuro_robin
-BUILD_PATH="$CUR_DIR/build"
+BUILD_PATH="$CUR_DIR"
 
 NEURO_ROBIN_PATH="$CUR_DIR/module"
 
 echo $BUILD_NAME
 
-./auto/configure --prefix=PAPATH_TO_NGINX_CONF --user=$USER --build=$BUILD_NAME --builddir=$BUILD_PATH \
-                 --with-http_ssl_module --with-http_perl_module=dynamic --with-stream=dynamic --with-stream_realip_module \
-                 --with-cpp_test_module --with-cc=gcc --with-cpp=gcc --with-cc-opt="-g" --with-ld-opt="" \
+./auto/configure --prefix=$PATH_TO_NGINX_CONF --user=$USER --build=$BUILD_NAME --builddir=$BUILD_PATH \
+                 --with-http_ssl_module --with-mail  --with-stream \
+                 --with-stream_realip_module --with-cpp_test_module --with-cc=gcc --with-cpp=gcc --with-cc-opt="-g" \
                  --with-cpu-opt=cpu --with-pcre --with-debug 
                 
-                # --add-module=$NEURO_ROBIN_PATH 
-                # --with-zlib=путь --with-zlib-opt=параметры --with-openssl=путь --with-openssl-opt=параметры 
+                # --add-module=$NEURO_ROBIN_PATH  
+                # --with-zlib=путь --with-zlib-opt=параметры --with-openssl=путь --with-openssl-opt=параметры --with-http_perl_module
